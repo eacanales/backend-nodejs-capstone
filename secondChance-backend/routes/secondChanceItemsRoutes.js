f00437c76beea8240a2c1e530d0068f28d434261
Your Name <you@example.com>
2025-01-27 19:46:40 -0500

    message

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -42,19 +42,29 @@ router.get('/', async (req, res, next) => {
 });
 
 // Add a new item
-//router.post('/', {Step 3: Task 6 insert code here}, async(req, res,next) => {
-//    try {
+router.post('/', upload.single('file'), async(req, res,next) => {
+    try {
 
         //Step 3: task 1 - insert code here
+        const db = await connectToDatabase();
         //Step 3: task 2 - insert code here
+        const collection = db.collection("secondChanceItems");
         //Step 3: task 3 - insert code here
+        let secondChanceItem = req.body;
         //Step 3: task 4 - insert code here
+        const lastItemQuery = await collection.find().sort({'id': -1}).limit(1);
+        await lastItemQuery.forEach(item => {
+        secondChanceItem.id = (parseInt(item.id) + 1).toString();
+        });
         //Step 3: task 5 - insert code here
-//        res.status(201).json(secondChanceItem.ops[0]);
-//    } catch (e) {
-//        next(e);
-//    }
-//});
+        const date_added = Math.floor(new Date().getTime() / 1000);
+        secondChanceItem.date_added = date_added
+        secondChanceItem = await collection.insertOne(secondChanceItem);
+        res.status(201).json(secondChanceItem.ops[0]);
+    } catch (e) {
+        next(e);
+    }
+});
 
 // Get a single secondChanceItem by ID
 router.get('/:id', async (req, res, next) => {
