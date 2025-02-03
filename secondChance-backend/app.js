9025b2699a0ccd6d05c06ec18e69f0e8a22544a1
eacanales <eacanales@outlook.com>
2025-02-03 10:08:27 -0500

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -8,17 +8,15 @@ const pinoLogger = require('./logger')
 const connectToDatabase = require('./models/db')
 // const { loadData } = require("./util/import-mongo/index");
 
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
+const app = express()
+app.use('*', cors())
+const port = 3060
 
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
+  pinoLogger.info('Connected to DB')
 })
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+  .catch((e) => console.error('Failed to connect to DB', e))
 
 app.use(express.json());
 
