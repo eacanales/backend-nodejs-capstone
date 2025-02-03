7f474af5b2b386d7d14ace40675dd94c68f6bc25
eacanales <eacanales@outlook.com>
2025-02-03 10:30:33 -0500

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -12,32 +12,31 @@ const directoryPath = 'public/images'
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, directoryPath); // Specify the upload directory
+    cb(null, directoryPath) // Specify the upload directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
-  },
-});
+    cb(null, file.originalname) // Use the original file name
+  }
+})
 
-const upload = multer({ storage: storage });
-
+const upload = multer({ storage: storage })
 
 // Get all secondChanceItems
 router.get('/', async (req, res, next) => {
-    logger.info('/ called');
-    try {
-        //Step 2: task 1 - insert code here
-        const db = await connectToDatabase();
-        //Step 2: task 2 - insert code here
-        //Step 2: task 3 - insert code here
-        //Step 2: task 4 - insert code here
+  logger.info('/ called')
+  try {
+    //Step 2: task 1 - insert code here
+    const db = await connectToDatabase();
+    //Step 2: task 2 - insert code here
+    //Step 2: task 3 - insert code here
+    //Step 2: task 4 - insert code here
 
-        const collection = db.collection("secondChanceItems");
-        const secondChanceItems = await collection.find({}).toArray();
-        res.json(secondChanceItems);
-    } catch (e) {
-        logger.console.error('oops something went wrong', e)
-        next(e);
+    const collection = db.collection("secondChanceItems");
+    const secondChanceItems = await collection.find({}).toArray();
+    res.json(secondChanceItems);
+  } catch (e) {
+      logger.console.error('oops something went wrong', e)
+      next(e);
     }
 });
 
