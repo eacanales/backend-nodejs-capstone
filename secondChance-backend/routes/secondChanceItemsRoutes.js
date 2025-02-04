68e0b451de71b388246b61a7268e887ab75c4ffc
eacanales <eacanales@outlook.com>
2025-02-04 08:31:53 -0500

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -59,32 +59,32 @@ router.post('/', upload.single('file'), async(req, res, next) => {
     const dateAdded = Math.floor(new Date().getTime() / 1000)
     secondChanceItem.date_added = dateAdded    
     secondChanceItem = await collection.insertOne(secondChanceItem)
-    console.log(secondChanceItem);
-      res.status(201).json(secondChanceItem);
+    console.log(secondChanceItem)
+    res.status(201).json(secondChanceItem)
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
 // Get a single secondChanceItem by ID
 router.get('/:id', async (req, res, next) => {
-    try {
-        // Step 4: task 1 - insert code here
-        const db = await connectToDatabase();
-        // Step 4: task 2 - insert code here
-        const collection = db.collection("secondChanceItems");
-        // Step 4: task 3 - insert code here
-        const id = req.params.id;
-        const secondChanceItem = await collection.findOne({ id });
-        // Step 4: task 4 - insert code here
-        if (!secondChanceItem) {
-            return res.status(404).send("secondChanceItem not found");
-          }
-        res.json(secondChanceItem);
-    } catch (e) {
-        next(e);
-    }
-});
+  try {
+    // Step 4: task 1 - insert code here
+    const db = await connectToDatabase();
+    // Step 4: task 2 - insert code here
+    const collection = db.collection("secondChanceItems");
+    // Step 4: task 3 - insert code here
+    const id = req.params.id;
+    const secondChanceItem = await collection.findOne({ id });
+    // Step 4: task 4 - insert code here
+    if (!secondChanceItem) {
+        return res.status(404).send("secondChanceItem not found");
+      }
+    res.json(secondChanceItem);
+  } catch (e) {
+    next(e)
+  }
+})
 
 // Update and existing item
 router.put('/:id', async(req, res,next) => {
