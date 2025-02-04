05bbd026d35fdd0814fda5f0cc9919d2614105ca
eacanales <eacanales@outlook.com>
2025-02-04 08:38:32 -0500

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -72,13 +72,13 @@ router.get('/:id', async (req, res, next) => {
     // Step 4: task 1 - insert code here
     const db = await connectToDatabase()
     // Step 4: task 2 - insert code here
-    const collection = db.collection('secondChanceItems');
+    const collection = db.collection('secondChanceItems')
     // Step 4: task 3 - insert code here
     const id = req.params.id
     const secondChanceItem = await collection.findOne({ id })
     // Step 4: task 4 - insert code here
     if (!secondChanceItem) {
-      return res.status(404).send("secondChanceItem not found");
+      return res.status(404).send('secondChanceItem not found')
     }
     res.json(secondChanceItem);
   } catch (e) {
@@ -87,7 +87,7 @@ router.get('/:id', async (req, res, next) => {
 })
 
 // Update and existing item
-router.put('/:id', async(req, res,next) => {
+router.put('/:id', async(req, res, next) => {
   try {
     //Step 5: task 1 - insert code here
     const db = await connectToDatabase();
