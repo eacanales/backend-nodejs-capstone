e9901d483a179e6314ffcdfdf606f51ae404f37e
eacanales <eacanales@outlook.com>
2025-02-04 09:03:16 -0500

    Update secondChanceItemsRoutes.js

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -115,14 +115,14 @@ router.put('/:id', async (req, res, next) => {
     )
     // Step 5: task 5 - insert code here
     if (updatepreloveItem) {
-      res.json({ 'uploaded':'success' });
+      res.json({ 'uploaded':'success' })
     } else {
-      res.json({ 'uploaded':'failed' });
+      res.json({ 'uploaded':'failed' })
     }
-    } catch (e) {
+  } catch (e) {
       next(e);
     }
-});
+})
 
 // Delete an existing item
 router.delete('/:id', async(req, res,next) => {
