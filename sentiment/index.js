22ac7b885f5ac41fa828ecf3671da9a0359a0114
eacanales <eacanales@outlook.com>
2025-02-05 09:23:58 -0500

    Update index.js

--- sentiment/index.js
+++ sentiment/index.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const logger = require('./logger');
-const expressPino = require('express-pino-logger')({ logger });
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const logger = require('./logger')
+const expressPino = require('express-pino-logger')({ logger })
 // Task 1: import the natural library
-const natural = require("natural");
+const natural = require('natural')
 
 // Task 2: initialize the express server
-const app = express(); //{{insert code here}}
+const app = express() // {{insert code here}}
 
 const port = process.env.PORT || 3000;
 
