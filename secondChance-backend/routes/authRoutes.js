d27ebb1009fd809a46e6d0d591de2b9badca3748
eacanales <eacanales@outlook.com>
2025-02-05 10:16:09 -0500

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -53,29 +53,29 @@ router.post('/register', async (req, res) => {
     logger.info('User registered successfully')
     res.json({ authtoken,email })
   } catch (e) {
-    logger.error(e);
+    logger.error(e)
     return res.status(500).send('Internal server error')
   }
-});    
+})    
 
 // Login Endpoint
 router.post('/login', async (req, res) => {
-    console.log("\n\n Inside login")
+    console.log('\n\n Inside login')
 
-    try {
-        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-        const db = await connectToDatabase();
-        // Task 2: Access MongoDB `users` collection
-        const collection = db.collection("users");
-        // Task 3: Check for user credentials in database
-        const theUser = await collection.findOne({ email: req.body.email });
-        // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
-        if (theUser) {
-            let result = await bcryptjs.compare(req.body.password, theUser.password)
-            if(!result){
-                logger.error('Passwords do not match');
-                return res.status(404).json({ error: 'Wrong password' });
-          }
+  try {
+    // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+    const db = await connectToDatabase();
+    // Task 2: Access MongoDB `users` collection
+    const collection = db.collection("users");
+    // Task 3: Check for user credentials in database
+    const theUser = await collection.findOne({ email: req.body.email });
+    // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password)
+      if(!result){
+        logger.error('Passwords do not match');
+        return res.status(404).json({ error: 'Wrong password' });
+    }
 
         // Task 5: Fetch user details from a database  
             let payload = {
