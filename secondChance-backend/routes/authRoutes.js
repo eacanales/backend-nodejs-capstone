1711416437157dc726635ee3d19b0c35cff4623a
eacanales <eacanales@outlook.com>
2025-02-05 10:19:09 -0500

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -60,19 +60,19 @@ router.post('/register', async (req, res) => {
 
 // Login Endpoint
 router.post('/login', async (req, res) => {
-    console.log('\n\n Inside login')
+  console.log('\n\n Inside login')
 
   try {
     // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
-    const db = await connectToDatabase();
+    const db = await connectToDatabase()
     // Task 2: Access MongoDB `users` collection
-    const collection = db.collection("users");
+    const collection = db.collection('users')
     // Task 3: Check for user credentials in database
-    const theUser = await collection.findOne({ email: req.body.email });
+    const theUser = await collection.findOne({ email: req.body.email })
     // Task 4: Check if the password matches the encrypted password and send appropriate message on mismatch
     if (theUser) {
-      let result = await bcryptjs.compare(req.body.password, theUser.password)
-      if(!result){
+      const result = await bcryptjs.compare(req.body.password, theUser.password)
+      if (!result){
         logger.error('Passwords do not match');
         return res.status(404).json({ error: 'Wrong password' });
     }
