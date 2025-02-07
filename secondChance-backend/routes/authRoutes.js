570f6184e43e15f4fec233e023b82a7f785ad1f7
eacanales <eacanales@outlook.com>
2025-02-07 08:17:56 -0500

    Update authRoutes.js

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -113,20 +113,19 @@ router.put('/update', async (req, res) => {
       logger.error('Email not found in the request headers')
       return res.status(400).json({ error: 'Email not found in the request headers' })
     }
-    
     // Task 4: Connect to MongoDB
-        const db = await connectToDatabase();
-        const collection = db.collection("users");
+    const db = await connectToDatabase()
+    const collection = db.collection('users')
 
     // Task 5: Find the user credentials in database
-        const existingUser = await collection.findOne({ email });
+    const existingUser = await collection.findOne({ email });
         
-        if (!existingUser) {
-            logger.error('User not found');
-            return res.status(404).json({ error: "User not found" });
-        }
-        existingUser.firstName = req.body.name;
-        existingUser.updatedAt = new Date();
+    if (!existingUser) {
+     logger.error('User not found');
+     return res.status(404).json({ error: 'User not found' });
+    }
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
         
     // Task 6: Update the user credentials in the database
         const updatedUser = await collection.findOneAndUpdate(
